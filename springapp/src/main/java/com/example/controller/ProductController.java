8e188cbce88ada6d92b0af6b690025c8b84d26d6
Iamneo <techadmin@iamneo.ai>
2023-10-05 11:28:46 +0000

    Backup Commit

--- springapp/src/main/java/com/example/controller/ProductController.java
+++ springapp/src/main/java/com/example/controller/ProductController.java
@@ -1,13 +1,42 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+import com.exa
 
 @RestController
-public class ProductController{
+@RequestMapping("/products")
+public class ProductController {
 
-    @GetMapping("/products/{id}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
-    public 
+    private final Map<Long, Product> productMap = new HashMap<>();
+    private long currentId = 1;
+
+    // Retrieve product details by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productMap.get(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Create a new product
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product newProduct) {
+        if (newProduct.getId() != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        newProduct.setId(currentId++);
+        productMap.put(newProduct.getId(), newProduct);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+    }
 }
--- springapp/src/main/java/com/example/service/ProductService.java
+++
@@ -1,6 +0,0 @@
-package com.example.service;
-import com.example.model.Product;
-
-public interface ProductService{
-
-}
