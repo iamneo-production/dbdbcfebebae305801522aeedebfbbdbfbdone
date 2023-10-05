a529c66ed9f15a41c22815d0b10959867cd44e93
Iamneo <techadmin@iamneo.ai>
2023-10-05 11:13:46 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/model/Product.java
@@ -0,0 +1,24 @@
+package com.example.model;
+
+public class Product{
+    String name;
+    float price;
+    int quantity;
+    void setName(String name){
+        this.name=name;
+    }
+    void setPrice(float price){
+        this.price=price;
+    }
+    void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+
+    String getName(){
+        return name;
+    }
+    float getPrice(){
+        return price;
+    }
+    int 
+}
